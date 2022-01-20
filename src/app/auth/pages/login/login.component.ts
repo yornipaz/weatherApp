dfd5baa69cb06b3f0a2b8f1dc2da6240bc7d9ed5
yornipaz <pazfelipe1996@hotmail.com>
2022-01-20 01:07:29 -0500

    Validacion de formularios de registro y login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
+  loginForm:FormGroup=this.fb.group({
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,Validators.minLength(8)]]
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb:FormBuilder) { }
+  login(event: Event){
+    event.preventDefault();
+    console.log(this.loginForm.value);
+    console.log(this.loginForm.valid)
   }
+
+ 
 
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
 
-  constructor() { }
+  registerForm:FormGroup=this.fb.group({
+    name:['',[Validators.required,Validators.minLength(8)]],
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,Validators.minLength(8)]]
+  });
 
-  ngOnInit(): void {
+  constructor(private fb:FormBuilder) { }
+  register(event: Event){
+    event.preventDefault();
+    console.log(this.registerForm.value);
+    console.log(this.registerForm.valid)
   }
 
 }
